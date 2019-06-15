147962f9419b1115ea4e353662a91a972e624f0c
wongricky <50374597+wongricky@users.noreply.github.com>
2019-06-15 17:40:10 -0500

    Update Ricky_Wong_FizzBuzzTest.java

--- Ricky_Wong_FizzBuzzTest.java
+++ Ricky_Wong_FizzBuzzTest.java
@@ -1,16 +1,24 @@
 package fizzBuzz;
 
+import java.util.Scanner;
+
 public class FizzBuzzTest {
 
 	public static void main(String[] args) {
 		
 		// TODO: get number // if number %3 == 0 - Fizz // if number %5 == 0 - Buzz // if %3 && %5 == 0 - FizzBuzz // else return number
 		
-		FizzBuzz new1 = new FizzBuzz();
-		String answer = new1.getString();
-		System.out.println(answer);
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter a number: ");
+		int value = in.nextInt();
+		FizzBuzz fb = new FizzBuzz();
+		fb.setValue(value);
+		in.close();
 		
+		String answer = fb.getString();
+		System.out.println(answer);
 		
 	}
 
 }
+
